b439d158d8a188962d6ea79e78c44dc42f355d25
miki <silmeria.c@gmail.com>
2014-10-12 21:55:42 +0900

    フッター修正

--- footer.php
+++ footer.php
@@ -1,10 +1,10 @@
 <div id="footerMain">
 	<ul>
-		<li>心いき&nbsp;/&nbsp;</li>
-		<li><a href="personal.php">個人情報</a>&nbsp;/&nbsp;</li>
-		<li><a href="policy.php">サイトポリシー</a>&nbsp;/&nbsp;</li>
-		<li><a href="disclamer.php">免責事項</a>&nbsp;/&nbsp;</li>
+		<li>心いき</li>
+		<li><a href="personal.php">個人情報</a></li>
+		<li><a href="policy.php">サイトポリシー</a></li>
+		<li><a href="disclamer.php">免責事項</a></li>
 		<li>動作環境</li>
 	</ul><br>
-	<p>&copy;Copyright&nbsp;COMMU-KEN!&nbsp;All&nbsp;rights&nbsp;reserved.</p>
+	<p>&copy;Copyright COMMU-KEN! All rights reserved.</p>
 </div>
